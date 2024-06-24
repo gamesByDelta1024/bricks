87e32e3ea63b28989b1e144e5f00cdd17b0a3bb2
Jake Stannix <jakestannix@gmail.com>
2024-06-24 13:34:31 -0600

    added brick spawn logic and brick draw logic

--- src/main.zig
+++ src/main.zig
@@ -14,21 +14,32 @@ const Bullet = struct {
     pos: Vector2,
     active: bool = false,
 };
+const Brick = struct {
+    pos: Vector2,
+    active: bool = false,
+};
 const MAX_BULLETS: usize = 8;
+const MAX_BRICKS: usize = 3;
 const FPS: i32 = 60;
 pub fn main() !void {
     const screen = struct {
         pub const width: i32 = 800;
         pub const height: i32 = 450;
     };
+
     const message = "Bullets: {d}/8";
     const message_buff = blk: {
         const bufflen = std.fmt.count(message, .{MAX_BULLETS}) + 1;
         const buf = try std.heap.page_allocator.alloc(u8, @intCast(bufflen));
         break :blk buf;
     };
+    defer std.heap.page_allocator.free(message_buff);
+
     var bullets: [MAX_BULLETS]Bullet = [_]Bullet{.{ .pos = .{ 0, 0 } }} ** MAX_BULLETS;
     var bullet_count: usize = 0;
+
+    var bricks: [MAX_BRICKS]Brick = [_]Brick{.{ .pos = .{ 0, 0 } }} ** MAX_BRICKS;
+
     var player: Player = .{
         .pos = .{
             (screen.width / 2) - 16,
@@ -40,9 +51,24 @@ pub fn main() !void {
     const bullet_speed: f32 = 4;
     raylib.initWindow(screen.width, screen.height, NAME);
     defer raylib.closeWindow();
-    raylib.setTargetFps(60);
-
-    while (!raylib.windowShouldClose()) {
+    raylib.setTargetFps(FPS);
+    var frame: i32 = 0;
+    var tick: i32 = 0;
+    while (!raylib.windowShouldClose()) : (frame += 1) {
+        if (@mod(frame, FPS) == 0)
+            tick += 1;
+        // out of bounds check
+        for (&bullets) |*bullet| {
+            const min_out: Vector2 = .{ 0, 0 };
+            const max_out: Vector2 = .{ screen.width, screen.height };
+            const min_ck = bullet.pos < min_out;
+            const max_ck = bullet.pos > max_out;
+            if (bullet.active and (min_ck[0] or min_ck[1] or max_ck[0] or max_ck[1])) {
+                bullet.active = false;
+                bullet_count -= 1;
+            }
+        }
+        // Player Input
         if (raylib.isKeyDown(.Right))
             player.pos[0] += player.speed;
         if (raylib.isKeyDown(.Left))
@@ -58,15 +84,23 @@ pub fn main() !void {
                 break :bullet;
             }
         }
-        // out of bounds check
-        for (&bullets) |*bullet| {
-            const min_out: Vector2 = .{ 0, 0 };
-            const max_out: Vector2 = .{ screen.width, screen.height };
-            const min_ck = bullet.pos < min_out;
-            const max_ck = bullet.pos > max_out;
-            if (bullet.active and (min_ck[0] or min_ck[1] or max_ck[0] or max_ck[1])) {
-                bullet.active = false;
-                bullet_count -= 1;
+        // spawn bricks
+        if (tick > 0 and @mod(tick, 3) == 0) {
+            tick = 0;
+            find_inactive: for (&bricks) |*brick| {
+                if (brick.active)
+                    continue;
+                brick.active = true;
+                brick.pos = .{ @floatFromInt(raylib.getRandomValue(0, screen.width)), 0 };
+                break :find_inactive;
+            }
+        }
+        // brick movement
+        if (@mod(frame, 10) == 0) {
+            for (&bricks) |*brick| {
+                if (!brick.active)
+                    continue;
+                brick.pos[1] += 2;
             }
         }
         // bullet movement
@@ -81,6 +115,9 @@ pub fn main() !void {
         for (bullets) |bullet|
             if (bullet.active)
                 raylib.drawRectangleV(bullet.pos, .{ 8, 8 }, Red);
+        for (bricks) |brick|
+            if (brick.active)
+                raylib.drawRectangleV(brick.pos, .{ 32, 32 }, raylib.colors.Blue);
 
         raylib.drawText(try std.fmt.bufPrintZ(message_buff, message, .{MAX_BULLETS - bullet_count}), 6, 6, 24, raylib.colors.Black);
     }
